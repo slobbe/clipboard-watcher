ae248f5d66aedcdd92c7035e8e29f0f01a264494
Sebastian Lobbe <slobbe@lobbe.cc>
2026-08-14 09:43:15 +0200

    Add extension metadata footer to preferences

--- src/prefs.js
+++ src/prefs.js
@@ -1,5 +1,6 @@
 import Adw from "gi://Adw";
 import Gtk from "gi://Gtk";
+import GLib from "gi://GLib";
 
 import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
@@ -16,6 +17,7 @@ const TIMEOUTS = [
 
 export default class ClipboardWatcherPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
+        const _ = this.gettext.bind(this);
         const settings = this.getSettings();
         const page = new Adw.PreferencesPage();
         const group = new Adw.PreferencesGroup({ title: "Privacy" });
@@ -35,8 +37,25 @@ export default class ClipboardWatcherPreferences extends ExtensionPreferences {
             settings.set_uint("clear-after-seconds", TIMEOUTS[row.selected][1]);
         });
 
+        const version = this.metadata["version-name"] ?? _("Development build");
+        const footer = new Gtk.Label({
+            label: `${GLib.markup_escape_text(this.metadata.name, -1)} v${GLib.markup_escape_text(version, -1)} · <a href="${GLib.markup_escape_text(this.metadata.url, -1)}">${_("GitHub")}</a>`,
+            use_markup: true,
+            xalign: 0.5,
+            yalign: 0.5,
+            justify: Gtk.Justification.CENTER,
+            margin_top: 6,
+            margin_bottom: 6,
+            wrap: true,
+        });
+        const footerGroup = new Adw.PreferencesGroup();
+
+        footer.add_css_class("dim-label");
+        footerGroup.add(footer);
+
         group.add(row);
         page.add(group);
+        page.add(footerGroup);
         window.add(page);
     }
 }
